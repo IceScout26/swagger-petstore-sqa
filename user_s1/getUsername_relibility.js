5d227c513c04831ef42f94b1b372874dd16996c5
DhimasTG <dhimasscout@gmail.com>
2025-03-02 20:21:39 +0700

    feat : get username relibility testing

---
+++ user_s1/getUsername_relibility.js
@@ -0,0 +1,42 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { Trend } from 'k6/metrics';
+
+// Metrik tambahan
+let responseTime = new Trend('response_time');
+
+export const options = {
+  vus: 30, // Jumlah VUs
+  duration: '10m', // Durasi pengujian
+};
+
+export default function () {
+  // Membuat username unik berdasarkan VU ID
+  const username = `user_${__VU}`;
+
+  const headers = { 'Content-Type': 'application/json' };
+
+  // Kirim request ke API
+  const res = http.get(
+      `https://petstore.swagger.io/v2/user/${username}`,
+      { headers }
+    );
+
+  // Menyimpan metrik tambahan
+  responseTime.add(res.timings.duration);
+
+  // console.log(`Response Status: ${res.status}`);
+  // console.log(`Response Body: ${res.body}`);
+
+  // Cek apakah response status 200
+  check(res, {
+    'Status is 200': (r) => r.status === 200,
+    'Status is 400': (r) => r.status === 400,
+    'Status is 429': (r) => r.status === 429,
+    'Status is 500': (r) => r.status === 500,
+    'Response time < 200ms': (r) => r.timings.duration < 200,
+    'Response time < 350ms': (r) => r.timings.duration < 350,
+    'Response time < 500ms': (r) => r.timings.duration < 500,
+    'Response time < 1000ms': (r) => r.timings.duration < 1000,
+  });
+}
