2f61c3f90b6e7c8252da0f4641df56d599bf6549
DhimasTG <dhimasscout@gmail.com>
2025-03-15 14:36:47 +0700

    feat : logout performance testing

---
+++ user_s1/logout_performance.js
@@ -0,0 +1,38 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { Trend } from 'k6/metrics';
+
+// Metrik tambahan
+let responseTime = new Trend('response_time');
+
+export const options = {
+  vus: 1650, // Jumlah VUs
+  iterations: 1650,
+};
+
+export default function () {
+  const url = `https://petstore.swagger.io/v2/user/logout`;
+
+  const headers = { 'Content-Type': 'application/json' };
+
+  // Kirim request ke API
+  const res = http.get(url, { headers });
+
+  // Menyimpan metrik tambahan
+  responseTime.add(res.timings.duration);
+
+  // console.log(`Response Status: ${res.status}`);
+  // console.log(`Response Body: ${res.body}`);
+
+  // Cek apakah response status 200
+  check(res, {
+    'Status is 200': (r) => r.status === 200,
+    'Status is 400': (r) => r.status === 400,
+    'Status is 429': (r) => r.status === 429,
+    'Status is 500': (r) => r.status === 500,
+    'Response time < 200ms': (r) => r.timings.duration < 200,
+    'Response time < 350ms': (r) => r.timings.duration < 350,
+    'Response time < 500ms': (r) => r.timings.duration < 500,
+    'Response time < 1000ms': (r) => r.timings.duration < 1000,
+  });
+}
