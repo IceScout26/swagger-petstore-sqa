e60f503a4dbe88e20f332ed5e63e17766aaa6be3
DhimasTG <dhimasscout@gmail.com>
2025-05-23 13:55:45 +0700

    feat : delete /pet/{petId} performance testing

---
+++ pet_s3/deletePetId_performance.js
@@ -0,0 +1,40 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { Trend } from 'k6/metrics';
+
+// Metrik tambahan
+let responseTime = new Trend('response_time');
+
+export const options = {
+  vus: 1600, // Jumlah VUs
+  iterations: 1600,
+};
+
+export default function () {
+  // Membuat petId unik berdasarkan VU ID
+  const petId = __VU;
+
+  const url = `https://petstore.swagger.io/v2/pet/${petId}`;
+
+  // Kirim request DELETE ke API
+  const res = http.del(url);
+
+  // Menyimpan metrik tambahan
+  responseTime.add(res.timings.duration);
+
+  // console.log(`Response Status: ${res.status}`);
+  // console.log(`Response Body: ${res.body}`);
+
+  // Cek apakah response status 200
+  check(res, {
+    'Status is 200': (r) => r.status === 200,
+    'Status is 400': (r) => r.status === 400,
+    'Status is 404': (r) => r.status === 404,
+    'Status is 429': (r) => r.status === 429,
+    'Status is 500': (r) => r.status === 500,
+    'Response time < 200ms': (r) => r.timings.duration < 200,
+    'Response time < 350ms': (r) => r.timings.duration < 350,
+    'Response time < 500ms': (r) => r.timings.duration < 500,
+    'Response time < 1000ms': (r) => r.timings.duration < 1000,
+  });
+}
